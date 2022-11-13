db1ac07c6bcb442926afd0fd3c959e9192036024
miintto <minjp93@gmail.com>
2022-11-13 21:02:11 +0900

    feat: 태그필터 UI 추가

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,6 +1,36 @@
-const requestDisplayPlaces = () => {
+const requestDisplayPlaces = (queryParams) => {
   $.ajax({
     url: "/api/place",
+    type: "GET",
+    dataType: "JSON",
+    data: queryParams,
+    beforeSend: (xhr) => {
+      const token = getAuthToken();
+      xhr.setRequestHeader("Authorization", "JWT " + token)
+    },
+    success: (data) => {
+      const places = data["data"]
+      var markerSource = new ol.source.Vector();
+      for (let i = 0; i < places.length; i++) {
+        feature = createFeature(places[i])
+        markerSource.addFeature(feature);
+      }
+      markerLayer = new ol.layer.Vector({
+        name: 'Marker',
+        source: markerSource,
+        style: markerStyle,
+      });
+      map.addLayer(markerLayer);
+    },
+    fail: (err) => {
+      console.log(err);
+    }
+  });
+};
+
+const requestTagList = () => {
+  $.ajax({
+    url: "/api/tag",
     type: "GET",
     dataType: "JSON",
     beforeSend: (xhr) => {
@@ -8,16 +38,18 @@ const requestDisplayPlaces = () => {
       xhr.setRequestHeader("Authorization", "JWT " + token)
     },
     success: (data) => {
-      const places = data["data"]
-      for (let i = 0; i < places.length; i++) {
-        feature = createFeature(places[i])
-        markerSource.addFeature(feature);
-      }
-      markerLayer = new ol.layer.Vector({
-        source: markerSource,
-        style: markerStyle,
+      let tags = `
+      <div id="filter-tag-all" class="filter-tag-section filter-tag-active" onclick="choiceFilterTag(event)">
+        <p>전체 선택</p>
+      </div>`;
+      data["data"].forEach((data) => {
+        tags = tags + `
+        <div class="filter-tag-section filter-tag-inactive filter-tag-normal" onclick="choiceFilterTag(event)">
+          <div class="filter-tag-id">` + data.id + `</div>
+          <p>#` + data.tag_name + `</p>
+        </div>`;
       });
-      map.addLayer(markerLayer);
+      $(".filter-tag-list").html(tags);
     },
     fail: (err) => {
       console.log(err);
--- static/js/map.js
+++ static/js/map.js
@@ -2,19 +2,19 @@ const map = new ol.Map({
   target: "map",
   layers: [
     new ol.layer.Tile({
+      name: 'Map',
       source: new ol.source.XYZ({
         url: "http://xdworld.vworld.kr:8080/2d/Base/202002/{z}/{x}/{y}.png"
-      })
+      }),
     })
   ],
   controls: [],
   view: new ol.View({
-    center: ol.proj.fromLonLat([126.95, 37.55]),
+    center: ol.proj.fromLonLat([126.98, 37.55]),
     zoom: 13,
   }),
 })
 
-var markerSource = new ol.source.Vector();
 var markerStyle = new ol.style.Style({
   image: new ol.style.Icon({
     opacity: 1,
@@ -38,9 +38,10 @@ map.on("click", (e) => {
   var feature = map.forEachFeatureAtPixel(e.pixel, (feature) => { return feature; });
   if (feature) {
     $("#place-info").show();
+    $("#filter").hide();
     $("#place-title").html(`<p>` + feature.get("place_name") + `</p>`);
     $("#place-description").html(`<p>` + feature.get("description") + `</p>`);
-    let tags = ""
+    let tags = "";
     feature.get("tags").forEach((e) => {
       tags = tags + `<p>#` + e.tag_name + `</p>`;
     });
@@ -49,7 +50,55 @@ map.on("click", (e) => {
 });
 
 $(".place-info-component").click((e) => {
-  $(".place-info").hide();
+  $("#place-info").hide();
+})
+
+$("#button-filter").click((e) => {
+  if ($(".filter-tag-list").find(".filter-tag-section").length == 0) {
+    requestTagList();
+  }
+  $("#place-info").hide();
+  $("#filter").show();
+})
+
+const deactivateTag = (target) => {
+  target.removeClass("filter-tag-active");
+  target.addClass("filter-tag-inactive");
+}
+
+const activateTag = (target) => {
+  target.removeClass("filter-tag-inactive");
+  target.addClass("filter-tag-active");
+}
+
+const choiceFilterTag = (e) => {
+  const target = $(e.currentTarget);
+  if (target.hasClass("filter-tag-active")) {
+    deactivateTag(target);
+  } else {
+    if (target.index(0) === 0) {
+      deactivateTag($(".filter-tag-normal"))
+    } else {
+      deactivateTag($("#filter-tag-all"));
+    }
+    activateTag(target);
+  }
+}
+
+$("#button-apply-filter").click((e) => {
+  let tagList = [];
+  $(".filter-tag-active").each((idx, tag) => {
+    tagList.push($(tag).find(".filter-tag-id").text());
+  });
+  map.getLayers().getArray()
+    .filter(layer => layer.get("name") === "Marker")
+    .forEach(layer => map.removeLayer(layer));
+  requestDisplayPlaces({tags: tagList.join(",")});
+  $("#filter").hide();
+})
+
+$("#button-cancel-filter").click((e) => {
+  $("#filter").hide();
 })
 
 $("#button-logout").click((e) => {
