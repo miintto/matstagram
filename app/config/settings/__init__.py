edecc345d300a2070f9df3829d69b7865d320bf9
miintto <minjp93@gmail.com>
2022-11-20 16:12:26 +0900

    feat: settings 파일 import 방식 변경

--- app/config/settings/__init__.py
+++ app/config/settings/__init__.py
@@ -1,24 +1,24 @@
 import os
 from functools import lru_cache
+import inspect
+from importlib import import_module
 
-from .base import Settings
-from .local import LocalSettings
-from .production import ProdSettings
-from .test import TestSettings
+from app.config.settings.base import Settings
 
 
 __all__ = ["get_settings"]
-
-
-_settings = {
-    "local": LocalSettings,
-    "production": ProdSettings,
-    "test": TestSettings,
-}
 
 
 @lru_cache()
 def get_settings() -> Settings:
     env = os.environ.get("APP_ENV", "local")
     print(f"APP_ENV:  {env}")
-    return _settings[env]()
+    for _, _settings in (
+        import_module(f".{env}", "app.config.settings").__dict__.items()
+    ):
+        if (
+            inspect.isclass(_settings)
+            and issubclass(_settings, Settings)
+            and _settings != Settings
+        ):
+            return _settings()
